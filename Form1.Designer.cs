84240b121fc98af5cbec26f9f79c192266828dde
Donald <14dseder@gmail.com>
2021-08-04 17:29:48 -0400

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿
+namespace IP_Viewer
+{
+    partial class lblSubnet
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(lblSubnet));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblAddress = new System.Windows.Forms.ListBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lstSubnet = new System.Windows.Forms.ListBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.lblGateway = new System.Windows.Forms.ListBox();
+            this.lblHostname = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblAddress);
+            this.groupBox1.Font = new System.Drawing.Font("Open Sans", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(13, 65);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(247, 130);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "IP Address";
+            // 
+            // lblAddress
+            // 
+            this.lblAddress.Font = new System.Drawing.Font("Open Sans", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAddress.FormattingEnabled = true;
+            this.lblAddress.ItemHeight = 15;
+            this.lblAddress.Location = new System.Drawing.Point(6, 19);
+            this.lblAddress.Name = "lblAddress";
+            this.lblAddress.Size = new System.Drawing.Size(235, 94);
+            this.lblAddress.TabIndex = 2;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lstSubnet);
+            this.groupBox2.Font = new System.Drawing.Font("Open Sans", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(13, 201);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(247, 118);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Subnet Mask";
+            // 
+            // lstSubnet
+            // 
+            this.lstSubnet.Font = new System.Drawing.Font("Open Sans", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstSubnet.FormattingEnabled = true;
+            this.lstSubnet.ItemHeight = 15;
+            this.lstSubnet.Location = new System.Drawing.Point(6, 17);
+            this.lstSubnet.Name = "lstSubnet";
+            this.lstSubnet.Size = new System.Drawing.Size(235, 94);
+            this.lstSubnet.TabIndex = 3;
+            this.lstSubnet.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.lblGateway);
+            this.groupBox3.Font = new System.Drawing.Font("Open Sans", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox3.Location = new System.Drawing.Point(13, 325);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(247, 132);
+            this.groupBox3.TabIndex = 0;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Gateway";
+            // 
+            // lblGateway
+            // 
+            this.lblGateway.Font = new System.Drawing.Font("Open Sans", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGateway.FormattingEnabled = true;
+            this.lblGateway.ItemHeight = 15;
+            this.lblGateway.Location = new System.Drawing.Point(6, 19);
+            this.lblGateway.Name = "lblGateway";
+            this.lblGateway.Size = new System.Drawing.Size(235, 94);
+            this.lblGateway.TabIndex = 3;
+            // 
+            // lblHostname
+            // 
+            this.lblHostname.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblHostname.Font = new System.Drawing.Font("Open Sans", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHostname.Location = new System.Drawing.Point(19, 27);
+            this.lblHostname.Name = "lblHostname";
+            this.lblHostname.Size = new System.Drawing.Size(235, 35);
+            this.lblHostname.TabIndex = 2;
+            this.lblHostname.Text = "label1";
+            this.lblHostname.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblSubnet
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(278, 469);
+            this.Controls.Add(this.lblHostname);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "lblSubnet";
+            this.Text = "IP Viewer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label lblHostname;
+        private System.Windows.Forms.ListBox lblAddress;
+        private System.Windows.Forms.ListBox lstSubnet;
+        private System.Windows.Forms.ListBox lblGateway;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.NetworkInformation;
+
+namespace IP_Viewer
+{
+    public partial class lblSubnet : Form
+    {
+        string hostname = Dns.GetHostName();
+        NetworkInterface[] nics = NetworkInterface.GetAllNetworkInterfaces();
+        IPGlobalProperties properties = IPGlobalProperties.GetIPGlobalProperties();
+
+        public lblSubnet()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblHostname.Text = hostname.ToUpper();
+
+            //var addresses = Dns.GetHostAddresses(Dns.GetHostName());
+            var addresses = NetworkInterface.GetAllNetworkInterfaces();
+            foreach (var ip in addresses)
+            {
+                Console.WriteLine(ip.Name.ToString());
+                Console.WriteLine(ip.OperationalStatus.ToString());
+                
+                
+                
+
+            }
+
+            int x = 0;
+            foreach (NetworkInterface adapter in nics)
+            {
+                lblGateway.Items.Add("--" + adapter.Name + "--");
+                lstSubnet.Items.Add("--" + adapter.Name + "--");
+                lblAddress.Items.Add("--" + adapter.Name + "--");
+
+                IPInterfaceProperties adapterProperties = adapter.GetIPProperties();
+                foreach (var Gateway in adapterProperties.GatewayAddresses) 
+                {
+                    
+                    lblGateway.Items.Add(Gateway.Address.ToString());               
+                }
+                
+                foreach (var Subnet  in adapterProperties.UnicastAddresses)
+
+                {
+                    
+                    lstSubnet.Items.Add(Subnet.IPv4Mask);
+                }
+                foreach (var addressip in adapterProperties.UnicastAddresses)
+                {
+                    
+                    lblAddress.Items.Add(addressip.Address);
+
+                }
+               
+            }
+
+           
+
+
+
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
